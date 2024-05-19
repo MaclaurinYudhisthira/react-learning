ad31aa62e244e985317732e22a0bead96dfd38b1
Kishan Dasondhi <kishan@switchon.io>
2024-05-19 19:39:08 +0530

    Add Backend and fetch api

---
+++ backend_react_practice_2/app.py
@@ -0,0 +1,84 @@
+from fastapi import FastAPI
+import uvicorn
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+
+app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+    expose_headers=["*"],
+)
+
+
+tasks = [
+    {"text": "Task 1", "id": 0, "day": "5 Fab 2024", "reminder": True},
+    {"text": "Task 2", "id": 1, "day": "5 Fab 2024", "reminder": True},
+    {"text": "Task 3", "id": 2, "day": "5 Fab 2024", "reminder": False},
+]
+
+
+class Task(BaseModel):
+    text: str
+    day: str
+    reminder: bool
+
+    
+class UpdateTask(BaseModel):
+    id:int
+    text: str = None
+    day: str = None
+    reminder: bool = None
+
+@app.get("/")
+def home():
+    return {"message": "Service is running"}
+
+
+@app.get("/tasks")
+def get_tasks():
+    return tasks
+
+
+@app.get("/tasks/{task_id}")
+def get_task(task_id: int):
+    return [task for task in tasks if task.get("id") == task_id][0]
+
+
+@app.post("/tasks")
+def add_task(task: Task):
+    global tasks
+    task = task.dict()
+    task["id"] = len(tasks)
+    tasks.append(task)
+    return task
+
+@app.put("/tasks")
+def update_task(updateTask: UpdateTask):
+    global tasks
+    updateTask = updateTask.dict(exclude_none=True)
+    
+    for idx, task in enumerate(tasks):
+        if task.get("id") == updateTask.get("id"):
+            for key, value in updateTask.items()
+                tasks[idx][key]=value
+            return task
+    
+    return {}
+
+
+@app.delete("/tasks/{task_id}")
+def delete_task(task_id: int):
+    global tasks
+    tasks = [task for task in tasks if task.get("id") != task_id]
+    return True
+
+
+if __name__ == "__main__":
+    uvicorn.run(app=app)
--- react_practice_2/src/App.jsx
+++ react_practice_2/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
@@ -6,36 +6,86 @@ import AddTask from "./components/AddTask";
 
 function App() {
 	const [showAddTask, setShowAddTask] = useState(false);
-	const [tasks, setTasks] = useState([
-		{ text: "Task 1", id: 1, day: "5 Fab 2024", reminder: true },
-		{ text: "Task 3", id: 2, day: "5 Fab 2024", reminder: true },
-		{ text: "Task 2", id: 3, day: "5 Fab 2024", reminder: false },
-	]);
+	const [tasks, setTasks] = useState([]);
+
+	useEffect(() => {
+		const getTasks = async () => {
+			const data = await fetchTasks();
+			setTasks(data);
+		};
+
+		getTasks();
+	}, []);
+
+	// Fetch Tasks
+	const fetchTasks = async () => {
+		const res = await fetch("http://localhost:8000/tasks");
+		const data = await res.json();
+		return data;
+	};
+
+
+	// Fetch Task
+	const fetchTask = async (id) => {
+		const res = await fetch(`http://localhost:8000/tasks/${id}`);
+		const data = await res.json();
+		return data;
+	};
 
 	// Save task
-	const onSaveTask = (task) => {
-		const id = Math.floor(Math.random() * 10000) + 1;
-		const newTask = { id, ...task };
+	const onSaveTask = async (task) => {
+		// const id = Math.floor(Math.random() * 10000) + 1;
+		// const newTask = { id, ...task };
+		const res = await fetch("http://localhost:8000/tasks",{
+			method:"POST",
+			headers:{
+				'Content-type':'application/json'
+			},
+			body:JSON.stringify(task)
+		});
+		const newTask = await res.json()
 		setTasks([...tasks, newTask]);
 	};
 
 	// Delete a task
-	const onDelete = (id) => {
-		setTasks(tasks.filter((task) => task.id !== id));
+	const onDelete = async (id) => {
+		const res = await fetch(`http://localhost:8000/tasks/${id}`,{method:"DELETE"})
+		const success = await res.json()
+		if (success==true)
+		{
+			setTasks(tasks.filter((task) => task.id !== id));
+		}
+
 	};
 
 	// Toggle the reminder of a task
-	const onToggle = (id) => {
+	const onToggle = async (id) => {
+		const taskToUpdate =await fetchTask(id)
+		const updatedTask = { ...taskToUpdate, reminder: !taskToUpdate.reminder }
+
+		const res  = await fetch(`http://localhost:8000/tasks`,{
+			method:"PUT",
+			headers:{
+				'Content-type':'application/json'
+			},
+			body:JSON.stringify(updatedTask)
+		});
+
+		const data = await res.json()
+
 		setTasks(
 			tasks.map((task) =>
-				task.id === id ? { ...task, reminder: !task.reminder } : task
+				task.id === id ? { ...task, reminder: !data.reminder } : task
 			)
 		);
 	};
 
 	return (
 		<div className="container">
-			<Header onAdd={() => setShowAddTask(!showAddTask)} showAddTask={showAddTask} />
+			<Header
+				onAdd={() => setShowAddTask(!showAddTask)}
+				showAddTask={showAddTask}
+			/>
 			{showAddTask && <AddTask onSaveTask={onSaveTask} />}
 			{tasks.length > 0 ? (
 				<Tasks tasks={tasks} onDelete={onDelete} onToggle={onToggle} />
